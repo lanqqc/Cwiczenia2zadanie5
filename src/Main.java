9e77c4d9a0dbe7e35e7b789baccfe345757b156a
Pavel <pavelnasennikk@gmail.com>
2022-10-14 16:26:13 +0200

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Need to continue this program at home! 
+        // Need to continue this program at home!
         double produktNetto = 9.99;
         double produktBrutto = 9.99 * 0.77;
         double
